acc86d72b11ad4bd5e4fa62ad35472629fbd273f
samueljuma <samueljuma1024@gmail.com>
2021-04-15 20:41:17 +0300

    if-statement

--- Guess_Number/src/guess_number/Guess_Number.java
+++ Guess_Number/src/guess_number/Guess_Number.java
@@ -1,14 +1,24 @@
 
 package guess_number;
 
+import java.util.Scanner;
+
 /**
  *
  * @author SamJay
  */
+
 public class Guess_Number {
 
     public static void main(String[] args) {
-       
+        Scanner in = new Scanner(System.in);
+        System.out.println("Guess a number");
+        int number = in.nextInt();
+        if (number == 7){
+            System.out.println("You guessed right");
+        }else{
+            System.out.println("Wrong guess");
+        }
     }
     
 }
