d365375fecea2823395d002a4384b3b369dc708f
samueljuma <samueljuma1024@gmail.com>
2021-04-15 21:00:31 +0300

    maximum guesses

--- Guess_Number/src/guess_number/Guess_Number.java
+++ Guess_Number/src/guess_number/Guess_Number.java
@@ -1,4 +1,3 @@
-
 package guess_number;
 
 import java.util.Scanner;
@@ -7,22 +6,28 @@ import java.util.Scanner;
  *
  * @author SamJay
  */
-
 public class Guess_Number {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
-        System.out.print("Guess a number:");
+
+        System.out.print("Guess a number:  ");
         int number = in.nextInt();
-        while (number != 0){
-        if (number == 7){
-            System.out.println("You guessed right");
-            return;
-        }else{
-            System.out.println("Wrong guess,(Enter 0 to exit). Try again:");
-            number =in.nextInt();
+        int count = 0;
+        while (number != 0) {
+            if (number == 7) {
+                System.out.println("You guessed right");
+                return;
+            }
+            if (count >= 4) {
+                System.out.println("\nSorry!! You reached a maximum number of guesses");
+                return;
+            } else {
+                System.out.print("Wrong guess,(Enter 0 to exit). Try again:  ");
+                number = in.nextInt();
+            }
+            count++;
         }
-    }}
-    
+    }
+
 }
