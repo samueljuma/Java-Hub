79c2c8c2e0c492dc46aff2791cb2527daee720c9
samueljuma <samueljuma1024@gmail.com>
2021-05-03 11:05:52 +0300

    java docs

--- AutomaticDocumentGeneration/src/automaticdocumentgeneration/AutomaticDocumentGeneration.java
+++ AutomaticDocumentGeneration/src/automaticdocumentgeneration/AutomaticDocumentGeneration.java
@@ -1,21 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package automaticdocumentgeneration;
 
+import java.util.Scanner;
+
 /**
- *
- * @author SamJay
+ * <h1> Count Number of Characters in a String!</h1>
+ * The program counts the number of characters and displays the result on the screen
+ * <p><b>Note: </b>It is also a sample of how to automatically generate java documentation in HTML</>
+ * @author Java_Pro
+ * @version 1.1
  */
 public class AutomaticDocumentGeneration {
-
+    
     /**
-     * @param args the command line arguments
+    * This is the main method. It is where the program begins
+    * it makes use of the countString method
+    * @param args Unused.
+    * @see Scanner Class
+    */
+
+    public static void main(String[] args){
+        System.out.println("\n\nCount the total number of words in a string: \n");
+        System.out.println("------------------------------------------\n");
+        System.out.println("Input the String: ");
+        
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        
+        System.out.println("Total number of words in the String is: " +countString (input));
+    }
+    
+    /**
+     * This method is used to iterate through the string while 
+     * counting the number of characters in the string
+     * @param word This is the parameter used as input for the method
+     * @return int This returns the number of characters in the string input
+     * @see <a href="https://www.javatpoint.com/java-for-loop">Loops in Java </a>
+     * @exception IndexOutOfBoundsException On Index out of bounds 
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    public static int countString(String word) {
+        int counter = 0;
+        
+        /* loop till end of String */
+        for(int i = 0; i < word.length(); i++){
+            
+            /* skip the space */
+            if(word.charAt(i) != ' '){
+                counter ++;
+            }
+        }
+        return counter;
     }
     
 }
