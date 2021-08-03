4114cd3aa44b6f6f6a01fa0037d69c5f12e83160
samueljuma <samueljuma1024@gmail.com>
2021-08-03 11:19:44 +0300

    multiples and ...

---
+++ New/src/com/devjay/Multiples.java
@@ -0,0 +1,26 @@
+package com.devjay;
+
+public class Multiples {
+
+    public static void main(String[] args) {
+        int n = 15;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Fizz");
+                }
+            }else{
+                if(i%5==0){
+                    System.out.println("Buzz");
+                }else{
+                    System.out.println(i);
+                }
+            }
+        }
+
+    }
+
+}
+
---
+++ New/src/com/devjay/Players.java
@@ -0,0 +1,9 @@
+package com.devjay;
+
+public class Players {
+
+    public static void main(String[] args) {
+        int k = 4;
+
+    }
+}
