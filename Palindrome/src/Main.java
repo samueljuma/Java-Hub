e9807e3cbcfbc12038d0c9112fde9f6396053700
samueljuma <samueljuma1024@gmail.com>
2024-01-02 10:51:50 +0300

    Recap - Palindrome

---
+++ Palindrome/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Print Program Desc
+        System.out.println("""
+                =====================================================
+                                       PALINDROME
+                =====================================================
+                The program checks whether the input from the user
+                is a palindrome prints the response back to the user
+                """);
+
+
+        //Take user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String userInput = scanner.nextLine();
+
+        //Show User What they entered
+        System.out.println("Your entered: " + userInput);
+
+        //Tell user if the String they entered is a Palindrome
+        displayResult(userInput,isPalindrome(userInput));
+    }
+
+     static boolean isPalindrome(String input){
+
+         StringBuilder reverse = new StringBuilder();
+
+         for(int i =input.length()-1;i>=0;i--){
+             reverse.append(input.charAt(i));
+         }
+
+         //Returns true or false
+         return reverse.toString().equals(input);
+
+    }
+
+    static void displayResult(String input, boolean isPalindrome){
+
+        if(isPalindrome){
+            System.out.printf("\n%s is a PALINDROME\n",input);
+        }else {
+            System.out.printf("\n%s is a NOT PALINDROME\n",input);
+        }
+    }
+
+}
